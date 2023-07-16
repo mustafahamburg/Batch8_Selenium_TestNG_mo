d80960b552a43e7650c69db176fe1e37b4d3fbef
mustafa <mstfozturk25hh@gmail.com>
2023-07-16 16:01:29 +0200

    wait1

--- src/test/java/com/eurotech/tests/day_01_automation_intro/_1_OpenBrowser.java
+++ src/test/java/com/eurotech/tests/day_01_automation_intro/_1_OpenBrowser.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _1_OpenBrowser {
+
     public static void main(String[] args) {
         // make selenium ready to talk with crome
         WebDriverManager.chromedriver().setup();
